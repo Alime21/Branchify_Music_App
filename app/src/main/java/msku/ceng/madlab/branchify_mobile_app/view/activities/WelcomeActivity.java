2bb703656f14958236068ed4489e41f981455ed9
KorkutKardes7 <korkutkardes7@hotmail.com>
2026-01-08 14:50:09 +0300

    Bug fixes.

--- app/src/main/java/msku/ceng/madlab/branchify_mobile_app/view/activities/WelcomeActivity.java
+++ app/src/main/java/msku/ceng/madlab/branchify_mobile_app/view/activities/WelcomeActivity.java
@@ -1,6 +1,8 @@
 package msku.ceng.madlab.branchify_mobile_app.view.activities;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +17,9 @@ import msku.ceng.madlab.branchify_mobile_app.R;
 
 public class WelcomeActivity extends AppCompatActivity {
 
+    private Handler handler;
+    private Runnable navigateRunnable;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,16 +30,37 @@ public class WelcomeActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // Auto-navigate to Splash1Activity after 0.5 seconds
+        handler = new Handler(Looper.getMainLooper());
+        navigateRunnable = this::navigateToSplash1;
+        handler.postDelayed(navigateRunnable, 500);
+    }
+
+    private void navigateToSplash1() {
+        if (handler != null) {
+            handler.removeCallbacks(navigateRunnable);
+        }
+        Intent intent = new Intent(WelcomeActivity.this, Splash1Activity.class);
+        startActivity(intent);
+        finish();
     }
 
     // when you click anywhere on the screen,it will go to Splash1Activity
     @Override
     public boolean onTouchEvent(MotionEvent event) {
         if (event.getAction() == MotionEvent.ACTION_DOWN) {
-            Intent intent = new Intent(WelcomeActivity.this, Splash1Activity.class);
-            startActivity(intent);
+            navigateToSplash1();
             return true;
         }
         return super.onTouchEvent(event);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (handler != null) {
+            handler.removeCallbacks(navigateRunnable);
+        }
     }
 }
